15a6de5969ba3e131885b122f223d3ccbe83e896
htomcat <thamada@yahoo-corp.jp>
2017-11-05 21:04:54 +0900

    [add]Custom cell & flowlayout

---
+++ InfiniteTabBar/InfiniteTabBar/InfiniteTabBarCell.swift
@@ -0,0 +1,13 @@
+//
+//  InfiniteTabBarCell.swift
+//  InfiniteTabBar
+//
+//  Created by htomcat on 2017/11/05.
+//  Copyright © 2017年 htomcat. All rights reserved.
+//
+
+import UIKit
+
+class InfiniteTabBarCell: UICollectionViewCell {
+    static let identifier = "InfiniteTabBarCell"
+}
---
+++ InfiniteTabBar/InfiniteTabBar/InfiniteTabBarFlowLayout.swift
@@ -0,0 +1,43 @@
+//
+//  InfiniteTabBarFlowLayout.swift
+//  InfiniteTabBar
+//
+//  Created by htomcat on 2017/11/05.
+//  Copyright © 2017年 htomcat. All rights reserved.
+//
+
+import UIKit
+
+class InfiniteTabBarFlowLayout: UICollectionViewFlowLayout {
+    private func itemsCount(at section: Int) -> Int {
+        guard let itemsCount = collectionView?.numberOfItems(inSection: section) else {
+            return 0
+        }
+        return itemsCount
+    }
+    private func itemsWidth() -> CGFloat {
+        return UIScreen.main.bounds.width / CGFloat(itemsCount(at: 0))
+    }
+    override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
+        var attributes = [UICollectionViewLayoutAttributes]()
+        guard itemsCount(at: 0) > 0 else {
+            return nil
+        }
+        for index in 0..<itemsCount(at: 0) {
+            let indexPath = IndexPath(row: index, section: 0)
+            guard let attribute = layoutAttributesForItem(at: indexPath) else {
+                break
+            }
+            attributes.append(attribute)
+        }
+        return attributes
+    }
+    override func layoutAttributesForItem(at indexPath: IndexPath) -> UICollectionViewLayoutAttributes? {
+        let attribute = UICollectionViewLayoutAttributes(forCellWith: indexPath)
+        attribute.frame = CGRect(x: itemsWidth() * CGFloat(indexPath.row),
+                                 y: 0,
+                                 width: itemsWidth(),
+                                 height: InfiniteTabBar.height)
+        return attribute
+    }
+}
