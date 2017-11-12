67a98466efa6f582c9afee5f242e2dc594f1dc75
htomcat <thamada@yahoo-corp.jp>
2017-11-12 21:15:05 +0900

    [add]Custom Tab Bar Item

---
+++ InfiniteTabBar/InfiniteTabBar/InfiniteTabBarItem.swift
@@ -0,0 +1,15 @@
+//
+//  InfiniteTabBarItem.swift
+//  InfiniteTabBar
+//
+//  Created by htomcat on 2017/11/11.
+//  Copyright © 2017年 htomcat. All rights reserved.
+//
+
+import UIKit
+
+struct InfiniteTabBarItem {
+    let title: String?
+    let image: UIImage?
+    let selectedImage: UIImage?
+}
